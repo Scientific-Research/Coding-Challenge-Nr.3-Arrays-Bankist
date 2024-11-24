ac88dd174d609a8a71c4262c85d71c54819c2f65
Maximilian <scientificresearch309@gmail.com>
2024-11-24 13:33:19 +0100

    adding math.fllor() and some more comments and improvement to the code

--- scripts.js
+++ scripts.js
@@ -11,13 +11,18 @@ TEST DATA 2: [16, 6, 10, 5, 6, 1, 4]
 console.log("----------Practice MAP, FILTER, REDUCE--------------------");
 
 const calcAverageHumanAge = (ages) =>
-  ages
-    .map((age, i, allAges) => (age <= 2 ? 2 * age : 16 + age * 4))
-    .filter((age) => age >= 18)
-    .reduce((acc, cur, i, allAges) => acc + cur / allAges.length, 0);
+  // all of three array methods here connected together using . and are considered as ONE statement, that's why we need nor CURLY BRACES neither RETRUN!
+
+  // NOTE: WE USE Math.floor() here, because the age 47.33 years old doesn't make any sense and floor() will convert it to the 47 years old!
+  Math.floor(
+    ages
+      .map((age, i, allAges) => (age <= 2 ? 2 * age : 16 + age * 4))
+      .filter((age) => age >= 18)
+      .reduce((acc, cur, i, allAges) => acc + cur / allAges.length, 0)
+  );
 
 const AverageAge1 = calcAverageHumanAge([5, 2, 4, 1, 15, 8, 3]);
-console.log(AverageAge1); // [36, 4, 32, 2, 76, 48, 28]
+console.log(AverageAge1); // 44
 
 const AverageAge2 = calcAverageHumanAge([16, 6, 10, 5, 6, 1, 4]);
-console.log(AverageAge2); // [80, 40, 56, 36, 40, 2, 32]
+console.log(AverageAge2); // 47
